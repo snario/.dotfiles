c8946324cd0140127a54b9f3fbb5830def91b07b
lihorne <lihorne@uwaterloo.ca>
2015-08-03 15:02:53 -0400

    rewrite entire dotfiles repo to new module style

---
+++ git/functions.zsh
@@ -0,0 +1,9 @@
+# Pull Request in one command
+# Example:
+#     mpr "Added sp"
+function mpr() {
+    git commit -a
+    branch=`git rev-parse --abbrev-ref HEAD`
+    git push -u origin $branch
+    hub pull-request -b develop
+}
